2622403c6e05f039a49b5d155cb8a27f26feda9f
RedBaron1918 <asatianikotiko1@gmail.com>
2022-10-07 03:57:18 +0400

    finally figured out why axios was not working

---
+++ pages/about/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about-page">
+    <h1>This is my absolutely fantastic "About Me" Page!</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex quidem aperiam similique libero. Numquam alias dolor
+      minus corrupti similique tenetur?
+    </p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex quidem aperiam similique libero. Numquam alias dolor
+      minus corrupti similique tenetur?
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
